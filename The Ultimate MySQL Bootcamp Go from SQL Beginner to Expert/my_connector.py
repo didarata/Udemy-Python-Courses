7a4931901a5b38595791f5d14515f9cde5f2c753
didarata <98163219+didarata@users.noreply.github.com>
2022-08-03 22:56:04 +0300

    Time to learn SQL and MYSQL

---
+++ The Ultimate MySQL Bootcamp Go from SQL Beginner to Expert/my_connector.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='3461',
+    port='3306',
+    database='python.test'
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute('SELECT * FROM users')
+
+users = mycursor.fetchall()
+
+for user in users:
+    print('Username ' + user[1])
+    print('Password ' + user[2])
