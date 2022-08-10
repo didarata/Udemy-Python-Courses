ffa80a06a1799561b3d86502e0f9c4904af2aefa
didarata <98163219+didarata@users.noreply.github.com>
2022-08-10 23:49:58 +0300

    Time to learn SQL and MYSQL, testing and messing around with pycharm and MySQL Workbench, and MySQL localhost.

--- The Ultimate MySQL Bootcamp Go from SQL Beginner to Expert/my_connector.py
+++ The Ultimate MySQL Bootcamp Go from SQL Beginner to Expert/my_connector.py
@@ -5,16 +5,16 @@ mydb = mysql.connector.connect(
     user='root',
     password='3461',
     port='3306',
-    database='baba'
+    database='python_test'
 )
 
 
-# mycursor = mydb.cursor()
-#
-# mycursor.execute('SELECT * FROM users')
-#
-# users = mycursor.fetchall()
-#
-# for user in users:
-#     print('Username ' + user[1])
-#     print('Password ' + user[2])
+mycursor = mydb.cursor()
+
+mycursor.execute('SELECT * FROM users')
+
+users = mycursor.fetchall()
+
+for user in users:
+    print(f"id: {user[0]}, Username: {user[1]}, Password: {user[2]}")
+    print(f'{"-" * len(users)}' * 17)
